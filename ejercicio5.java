0610d5bf19b6d11ac514396ee0e87602b0d3600a
valentinabt <vvalentinabelmonte@gmail.com>
2024-08-21 22:05:13 -0300

    al fin funciona

--- ejercicio5.java
+++ ejercicio5.java
@@ -1,15 +1,15 @@
 class ejercicio5 {
-//funciona para el upite pero al menos funciona 
+//priorizo que el exceso ea menor a que tener menor cantidad de billetes
 static int[] minimo(int[]a,int[]b) {
-    if(a[0] > b[0]) {
+    if(a[1] > b[1]) {
         return b ;
 
     }
-    if(a[0]<b[0]) {
+    if(a[1]<b[1]) {
         return a ;
     }
     else {
-        if(a[1]>b[1]) {
+        if(a[0]>b[0]) {
             return b ;
         }
         else {
@@ -32,12 +32,12 @@ static int[] cc(int[]conj,int i,int j) {
         res[1] = Integer.MAX_VALUE;
         return res ;
     }
-    /*if(j==0 ) {
+    if(j==0 ) {
         int[] res = new int[2] ;
-        res[0] = i ;
+        res[0] = 0 ;
         res[1] = 0 ;
         return res ;
-    } */
+    } 
 
 
     else {
@@ -49,7 +49,7 @@ static int[] cc(int[]conj,int i,int j) {
 
 }
 public static void main(String[] args) {
-    int[] prueba = {2, 3, 5, 15,20} ;
+    int[] prueba = {2, 3, 5,10,20,20} ;
     int[] res = cc(prueba,0,14) ;
     System.out.println(res[0]);
     System.out.println(res[1]);
