1dec4e75ddadc8aefa1382d6926d6d898d1b21c4
valentinabt <vvalentinabelmonte@gmail.com>
2024-08-21 21:58:42 -0300

    corregi cosaas

--- ejercicio5.java
+++ ejercicio5.java
@@ -22,7 +22,7 @@ static int[] minimo(int[]a,int[]b) {
 static int[] cc(int[]conj,int i,int j) {
     if(j<0) {
         int[] res = new int[2] ;
-        res[0] = i ;
+        res[0] = 0 ;
         res[1] = -j ;
         return res ;
     }
@@ -32,23 +32,24 @@ static int[] cc(int[]conj,int i,int j) {
         res[1] = Integer.MAX_VALUE;
         return res ;
     }
-    if(j==0 ) {
+    /*if(j==0 ) {
         int[] res = new int[2] ;
         res[0] = i ;
         res[1] = 0 ;
         return res ;
-    }
+    } */
 
 
     else {
         int[] usar = cc(conj, i+1, j-conj[i]) ;
+        usar[0]++ ;
         int[] noUsar = cc(conj, i+1, j) ;
         return minimo(usar,noUsar) ;
     }
 
 }
 public static void main(String[] args) {
-    int[] prueba = {2, 3, 5, 10, 20, 20} ;
+    int[] prueba = {2, 3, 5, 15,20} ;
     int[] res = cc(prueba,0,14) ;
     System.out.println(res[0]);
     System.out.println(res[1]);
