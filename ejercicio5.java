7647383a604322408bddd8448eff2e0af56b73be
valentinabt <vvalentinabelmonte@gmail.com>
2024-08-22 13:54:28 -0300

    al fin loco

--- ejercicio5.java
+++ ejercicio5.java
@@ -1,6 +1,8 @@
-class ejercicio5 {
+class ejercicio6 {
+    
 //priorizo que el exceso ea menor a que tener menor cantidad de billetes
 static int[] minimo(int[]a,int[]b) {
+
     if(a[1] > b[1]) {
         return b ;
 
@@ -19,7 +21,7 @@ static int[] minimo(int[]a,int[]b) {
   
 
 }
-static int[] cc(int[]conj,int i,int j) {
+static int[] optiPagoBT(int[]conj,int i,int j) {
     if(j<0) {
         int[] res = new int[2] ;
         res[0] = 0 ;
@@ -41,16 +43,73 @@ static int[] cc(int[]conj,int i,int j) {
 
 
     else {
-        int[] usar = cc(conj, i+1, j-conj[i]) ;
+        int[] usar = optiPagoBT(conj, i+1, j-conj[i]) ;
         usar[0]++ ;
-        int[] noUsar = cc(conj, i+1, j) ;
+        int[] noUsar = optiPagoBT(conj, i+1, j) ;
         return minimo(usar,noUsar) ;
     }
 
 }
+
+static int[] optiPago(int[]conj,int n,int k) {
+    int[][][] matriz = new int[conj.length][k+1][] ;
+    for(int l= 0;l<conj.length;l++) {
+        for(int p = 0; p<matriz[0].length ; p++) {
+            matriz[l][p] = new int[2] ;
+            matriz[l][p][0] = -1 ;
+            matriz[l][p][1] = -1;
+        }
+    }
+    return optiPagoPD(conj, n, k, matriz) ;
+    }
+    static int[] optiPagoPD(int[]conj,int i,int j,int[][][]matriz) {
+        if(j<0) {
+            int[] res = new int[2] ;
+            res[0] = 0 ;
+            res[1] = -j ;
+            
+            return res ;
+        }
+        if(j>0 && i == conj.length) {
+            int[] res = new int[2] ;
+            res[0] = Integer.MAX_VALUE;
+            res[1] = Integer.MAX_VALUE;
+           
+            return res ;
+        }
+        if(j==0 ) {
+            int[] res = new int[2] ;
+            res[0] = 0 ;
+            res[1] = 0 ;
+            
+            return res ;
+        } 
+    
+    
+        else { 
+           if(matriz[i][j][0] != -1 && matriz[i][j][1] != -1) {
+            return matriz[i][j] ;
+        }else {
+            int[] usar = optiPagoPD(conj, i+1, j-conj[i],matriz) ;
+            usar[0]++ ;
+            //aca tmb modifico la matriz!
+           // matriz[i][Math.abs(j-conj[i])][0]++ ;
+           
+            int[] noUsar = optiPagoPD(conj, i+1, j,matriz) ;
+            //matriz[i][Math.abs(j-conj[i])][0]++ ;
+
+            int[] res1 = minimo(usar,noUsar) ;
+            matriz[i][j][0] = res1[0];
+            matriz[i][j][1] = res1[1];
+            return res1 ;
+        }
+    }
+    
+    }
+
 public static void main(String[] args) {
-    int[] prueba = {2, 3, 5,10,20,20} ;
-    int[] res = cc(prueba,0,14) ;
+    int[] prueba = {1,2,3,5,10,20,20} ;
+    int[] res = optiPago(prueba,0,14) ;
     System.out.println(res[0]);
     System.out.println(res[1]);
 
